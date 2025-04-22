0882891d1654826ec0d796427e873d451456ff21
sandy102000 <sandyyoussef00@gmail.com>
2025-04-22 22:03:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,7 @@ import Signin from'./pages/Signin.jsx'
 import UserProvider from './Provider/UserProvider.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import AddTask from './pages/addtask.jsx'
-// import CircularProgress from '@mui/material/CircularProgress';
-// import Box from '@mui/material/Box';
-// export default function CircularIndeterminate() {
-//   return (
-//     <Box sx={{ display: 'flex' }}>
-//       <CircularProgress />
-//     </Box>
-//   )};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
